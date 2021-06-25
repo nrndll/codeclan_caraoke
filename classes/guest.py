4d0beb53f7877db82286df37b1b6020a0b651b1f
Nathan <nathan.rendall@gmail.com>
2021-06-25 15:11:40 +0100

    can_afford_entry_fee method added tp guest class, True and False tests added to guest_test

--- classes/guest.py
+++ classes/guest.py
@@ -3,3 +3,9 @@ class Guest:
     def __init__(self, name, wallet):
         self.name = name
         self.wallet = wallet
+
+    def can_afford_entry_fee(self, room):
+        if self.wallet >= room.entry_fee:
+            return True
+        else:
+            return False
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,13 +1,22 @@
 import unittest
 from classes.guest import *
+from classes.room import *
 
 class TestGuest(unittest.TestCase):
 
     def setUp(self):
         self.guest = Guest("Kiryu", 75.00)
+        self.room = Room("Onomichi", 5, 15.00)
 
     def test_guest_has_name(self):
         self.assertEqual("Kiryu", self.guest.name)
 
     def test_guest_has_wallet(self):
         self.assertEqual(75.00, self.guest.wallet)
+
+    def test_guest_can_afford_entry_fee__True(self):
+        self.assertEqual(True, self.guest.can_afford_entry_fee(self.room))
+
+    def test_guest_can_afford_entry_fee__False(self):
+        self.room = Room("Ryukyu", 25, 990.00)
+        self.assertEqual(False, self.guest.can_afford_entry_fee(self.room))

b82ed9effa403e508dd8772e37cd899ea1171c86
Nathan <nathan.rendall@gmail.com>
2021-06-25 12:14:51 +0100

    check_out_guest method added to room class and test_can_check_out_guest added to room_test

--- classes/room.py
+++ classes/room.py
@@ -6,3 +6,6 @@ class Room:
 
     def check_in_guest(self, guest):
         self.guests.append(guest)
+
+    def check_out_guest(self, guest):
+        self.guests.remove(guest)
--- tests/room_test.py
+++ tests/room_test.py
@@ -19,3 +19,8 @@ class TestRoom(unittest.TestCase):
     def test_can_check_in_guest(self):
         self.room.check_in_guest(self.guest)
         self.assertEqual(1, len(self.room.guests))
+
+    def test_can_check_out_guest(self):
+        self.room.check_in_guest(self.guest)
+        self.room.check_out_guest(self.guest)
+        self.assertEqual(0, len(self.room.guests))
